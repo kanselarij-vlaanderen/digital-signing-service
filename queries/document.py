aba09e6e9f89fa9d0d3cd3aa04579a332aaa7641
Michaël Dierick <michael.dierick@redpencil.io>
2020-12-02 18:07:36 +0100

    feat: add query template for inserting new documents

--- queries/document.py
+++ queries/document.py
@@ -7,6 +7,8 @@ from escape_helpers import sparql_escape_uri, sparql_escape_string, sparql_escap
 TIMEZONE = timezone('Europe/Brussels')
 APPLICATION_GRAPH = "http://mu.semte.ch/application"
 
+DOCUMENT_BASE_URI = "http://kanselarij.vo.data.gift/id/stukken/"
+
 def construct_get_file_for_document(document_uri, file_mimetype=None, graph=APPLICATION_GRAPH):
     if file_mimetype is not None:
         format_filter = "FILTER( CONTAINS( ?format, {} ) )".format(sparql_escape_string(file_mimetype))
@@ -35,3 +37,40 @@ LIMIT 1
         graph=sparql_escape_uri(graph),
         document=sparql_escape_uri(document_uri),
         format_filter=format_filter)
+
+def construct_insert_document(document_name, file_uri, graph=APPLICATION_GRAPH):
+    document = {
+        "name": document_name,
+        "created": datetime.now(TIMEZONE)
+    }
+    document["uuid"] = generate_uuid()
+    document["uri"] = DOCUMENT_BASE_URI + document["uuid"]
+    query_template = Template("""
+PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
+PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+INSERT {
+    GRAPH $graph {
+        $document a dossier:Stuk ;
+            mu:uuid $uuid ;
+            dct:title $name ;
+            dct:created $created ;
+            ext:file $file .
+    }
+}
+WHERE {
+    GRAPH $graph {
+        $file a nfo:FileDataObject .
+    }
+}
+""")
+    return query_template.substitute(
+        graph=sparql_escape_uri(graph),
+        document=sparql_escape_uri(document["uri"]),
+        uuid=sparql_escape_string(document["uuid"]),
+        name=sparql_escape_string(document["name"]),
+        created=sparql_escape_datetime(document["created"]),
+        file=sparql_escape_uri(file_uri))
