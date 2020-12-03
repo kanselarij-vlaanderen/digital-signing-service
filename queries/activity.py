d908a7a020a0c1662a7bc9daed40b28dca5db7c2
Michaël Dierick <michael.dierick@redpencil.io>
2020-12-03 14:50:48 +0100

    feat: add query-template for fetching a wrap-up activity

--- queries/activity.py
+++ queries/activity.py
@@ -298,3 +298,38 @@ WHERE {
         sh_document_id=sparql_escape_string(sh_document_id),
         signed_doc=sparql_escape_uri(signed_doc),
         end_time=sparql_escape_datetime(end_time))
+
+def construct_get_wrap_up_activity(sh_package_id,
+                                   sh_document_id,
+                                   graph=APPLICATION_GRAPH):
+    query_template = Template("""
+PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
+PREFIX prov: <http://www.w3.org/ns/prov#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX sh: <http://mu.semte.ch/vocabularies/ext/signinghub/>
+
+SELECT DISTINCT(?signing_wrap_up AS ?uri) ?signed_doc
+WHERE {
+    GRAPH $graph {
+        ?signing_prep a prov:Activity ;
+            dct:type $prep_type ;
+            sh:document ?sh_doc .
+        ?sh_doc sh:packageId $sh_package_id ;
+            sh:documentId $sh_document_id .
+        ?signing a prov:Activity ;
+            dct:type $type ;
+            prov:wasInformedBy ?signing_prep .
+        ?signing_wrap_up a prov:Activity ;
+            dct:type $wrap_up_type ;
+            prov:generated ?signed_doc .
+        ?signed_doc a dossier:Stuk .
+    }
+}
+""")
+    return query_template.substitute(
+        graph=sparql_escape_uri(graph),
+        prep_type=sparql_escape_uri(SIGNING_PREP_ACT_TYPE_URI),
+        sig_type=sparql_escape_uri(SIGNING_ACT_TYPE_URI),
+        wrap_up_type=sparql_escape_uri(SIGNING_WRAP_TYPE_URI),
+        sh_package_id=sparql_escape_string(sh_package_id),
+        sh_document_id=sparql_escape_string(sh_document_id))
