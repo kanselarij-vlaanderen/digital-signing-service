897a2c6d5ce85b9245aa4a845782db77720dfeb5
Michaël Dierick <michael.dierick@redpencil.io>
2020-12-03 10:55:39 +0100

    refactor: move file creation part to file lib + return doc meta dict

--- lib/document.py
+++ lib/document.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 from pytz import timezone
-from flask import g
 from helpers import query, generate_uuid
 from ..queries.document import construct_get_file_for_document, construct_insert_document
 from ..sudo_query import update as sudo_update
-from ..queries.file import construct_insert_file_query
+from .file import download_sh_doc_to_mu_file
 from .exceptions import NoQueryResultsException
 
 TIMEZONE = timezone('Europe/Brussels')
@@ -12,7 +10,7 @@ TIMEZONE = timezone('Europe/Brussels')
 APPLICATION_GRAPH = "http://mu.semte.ch/application"
 SIGNED_DOCS_GRAPH = "http://mu.semte.ch/graphs/organizations/kanselarij"
 
-FILE_BASE_URI = "http://kanselarij.vo.data.gift/id/files/"
+DOC_BASE_URI = "http://kanselarij.vo.data.gift/id/stukken/"
 
 def get_file_for_document(document_uri):
     query_string = construct_get_file_for_document(document_uri, file_mimetype="application/pdf")
@@ -23,28 +21,12 @@ def get_file_for_document(document_uri):
     return file
 
 def download_sh_doc_to_kaleidos_doc(sh_package_id, sh_document_id, document_name):
-    file_bytes = g.sh_session.download_document(sh_package_id, sh_document_id)
-    physical_file = {
-        "extension": "pdf",
-        "uuid": generate_uuid()
-    } # Other properties taken form virtual file
-    physical_file["name"] = physical_file["uuid"] + "." + physical_file["extension"]
-    physical_path = "/share/{}".format(physical_file["name"])
-    physical_file["uri"] = physical_path.replace("/share/", "share://")
-    with open(physical_path, "wb") as f:
-        f.write(file_bytes)
-    virtual_file = {
-        "created": datetime.now(TIMEZONE),
-        "extension": "pdf",
+    virtual_file = download_sh_doc_to_mu_file(sh_package_id, sh_document_id)
+    doc = {
         "uuid": generate_uuid(),
-        "mimetype": "application/pdf",
-        "size": len(file_bytes)
+        "name": document_name
     }
-    virtual_file["uri"] = FILE_BASE_URI + virtual_file["uuid"]
-    virtual_file["name"] = virtual_file["uuid"] + "." + virtual_file["extension"]
-    ins_f_query_string = construct_insert_file_query(virtual_file, physical_file, SIGNED_DOCS_GRAPH)
-    sudo_update(ins_f_query_string)
+    doc["uri"] = DOC_BASE_URI + doc["uuid"]
     ins_doc_query_string = construct_insert_document(document_name, virtual_file["uri"], SIGNED_DOCS_GRAPH)
     sudo_update(ins_doc_query_string)
-    return virtual_file
-
+    return doc
--- lib/file.py
+++ lib/file.py
@@ -1,9 +1,19 @@
+from datetime import datetime
+from pytz import timezone
 from flask import g
-from helpers import query
-from ..queries.file import construct_get_file_query, construct_get_file_by_id
+from helpers import query, generate_uuid
+from ..queries.file import construct_get_file_query, \
+    construct_get_file_by_id, \
+    construct_insert_file_query
 from .exceptions import NoQueryResultsException
+from ..sudo_query import update as sudo_update
 
 SH_SOURCE = "Kaleidos" # TODO https://manuals.ascertia.com/SigningHub-apiguide/default.aspx#pageid=1022
+
+TIMEZONE = timezone('Europe/Brussels')
+
+FILE_BASE_URI = "http://kanselarij.vo.data.gift/id/files/"
+SIGNED_FILES_GRAPH = "http://mu.semte.ch/graphs/organizations/kanselarij"
 
 def read_file_bytes(file_uri, max_file_size=None):
     file_query = construct_get_file_query(file_uri)
@@ -47,3 +57,27 @@ def add_file_to_sh_package(file_uri, sh_package_id):
                                                SH_SOURCE,
                                                convert_document=False)
     return sh_document
+
+def download_sh_doc_to_mu_file(sh_package_id, sh_document_id):
+    file_bytes = g.sh_session.download_document(sh_package_id, sh_document_id)
+    physical_file = {
+        "extension": "pdf",
+        "uuid": generate_uuid()
+    } # Other properties taken form virtual file
+    physical_file["name"] = physical_file["uuid"] + "." + physical_file["extension"]
+    physical_path = "/share/{}".format(physical_file["name"])
+    physical_file["uri"] = physical_path.replace("/share/", "share://")
+    with open(physical_path, "wb") as f:
+        f.write(file_bytes)
+    virtual_file = {
+        "created": datetime.now(TIMEZONE),
+        "extension": "pdf",
+        "uuid": generate_uuid(),
+        "mimetype": "application/pdf",
+        "size": len(file_bytes)
+    }
+    virtual_file["uri"] = FILE_BASE_URI + virtual_file["uuid"]
+    virtual_file["name"] = virtual_file["uuid"] + "." + virtual_file["extension"]
+    ins_f_query_string = construct_insert_file_query(virtual_file, physical_file, SIGNED_FILES_GRAPH)
+    sudo_update(ins_f_query_string)
+    return virtual_file
