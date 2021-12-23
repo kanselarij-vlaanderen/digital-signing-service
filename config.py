51a49ea959f9f0e62d4938e63b55bc38ba84988e
Michaël Dierick <michael.dierick@redpencil.io>
2021-12-23 14:47:15 +0100

    clarify resource base uri constants naming

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 __KANSELARIJ_URL = "http://kanselarij.vo.data.gift/"
-SIGNINGHUB_BASE_URI = os.environ.get("SIGNINGHUB_API_URL", __KANSELARIJ_URL).strip("/") + "/"
+SIGNINGHUB_RESOURCE_BASE_URI = os.environ.get("SIGNINGHUB_API_URL", __KANSELARIJ_URL).strip("/") + "/"
 
 APPLICATION_GRAPH = "http://mu.semte.ch/application"
 KANSELARIJ_GRAPH = "http://mu.semte.ch/graphs/organizations/kanselarij"
--- lib/uri.py
+++ lib/uri.py
@@ -1,10 +1,10 @@
 from .. import config as _config
 
 class __Resource:
-    __RESOURCE_BASE_URI = "http://themis.vlaanderen.be/id/"
+    __THEMIS_RESOURCE_BASE_URI = "http://themis.vlaanderen.be/id/"
 
     def __build (self, type: str, id: str):
-        return self.__RESOURCE_BASE_URI + type + "/" + id
+        return self.__THEMIS_RESOURCE_BASE_URI + type + "/" + id
 
     def piece(self, id: str):
         return self.__build("stuk", id)
@@ -19,6 +19,6 @@ class __Resource:
         return self.__build("handteken-handtekenactiviteit", id)
 
     def signinghub_document(self, package_id: str, document_id: str):
-        return f"{_config.SIGNINGHUB_BASE_URI}package/{package_id}/document/{document_id}"
+        return f"{_config.SIGNINGHUB_RESOURCE_BASE_URI}package/{package_id}/document/{document_id}"
 
 resource = __Resource()
