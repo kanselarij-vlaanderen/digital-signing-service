85f8e419af2c78ea61aa92139b792ff5c9b2334f
Michaël Dierick <michael.dierick@redpencil.io>
2022-01-06 15:13:22 +0100

    remove development mode test already provided by template

--- config.py
+++ config.py
@@ -9,12 +9,3 @@ TIMEZONE = timezone('Europe/Brussels')
 # Constants
 APPLICATION_GRAPH = "http://mu.semte.ch/application"
 KANSELARIJ_GRAPH = "http://mu.semte.ch/graphs/organizations/kanselarij"
-
-class __Mode:
-    __MODE = os.environ.get("MODE")
-
-    @property
-    def dev(self):
-        return self.__MODE == "development"
-
-mode = __Mode()
