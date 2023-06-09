a6e9e60747274279b1fb4abe5ecd49570ba89075
Sergio Fenoll <sergio@fenoll.be>
2023-06-09 15:02:13 +0200

    feat: add query to update approval start date

--- queries/signing_flow_approvers.py
+++ queries/signing_flow_approvers.py
@@ -29,6 +29,43 @@ WHERE {
     )
 
 
+def construct_update_approval_activity_start_date(signflow_uri: str, email, start_date, graph=APPLICATION_GRAPH) -> str:
+    query_template = Template("""
+PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
+PREFIX sign: <http://mu.semte.ch/vocabularies/ext/handtekenen/>
+
+DELETE {
+    GRAPH $graph {
+        ?approval_activity dossier:Activiteit.startdatum ?start_date .
+    }
+}
+INSERT {
+    GRAPH $graph {
+        ?approval_activity dossier:Activiteit.startdatum $start_date .
+    }
+}
+WHERE {
+    GRAPH $graph {
+        $signflow a sign:Handtekenaangelegenheid ;
+            sign:doorlooptHandtekening ?sign_subcase .
+        ?sign_subcase a sign:HandtekenProcedurestap ;
+            ^sign:goedkeuringVindtPlaatsTijdens ?approval_activity .
+        ?approval_activity a sign:Goedkeuringsactiviteit ;
+            sign:goedkeurder $approver .
+        OPTIONAL {
+            ?approval_activity dossier:Activiteit.startdatum ?start_date .
+        }
+    }
+}
+""")
+    return query_template.substitute(
+        graph=sparql_escape_uri(graph),
+        signflow=sparql_escape_uri(signflow_uri),
+        approver=sparql_escape_uri(email),
+        start_date=sparql_escape_datetime(start_date)
+    )
+
+
 def construct_update_approval_activity_end_date(signflow_uri: str, email, end_date, graph=APPLICATION_GRAPH) -> str:
     query_template = Template("""
 PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
