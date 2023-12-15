07088bc4180d456e887a9200769eff3d331479f3
Sergio Fenoll <sergio@fenoll.be>
2023-12-15 17:04:21 +0100

    chore: extract SH session creation logic from decorator
    This way other parts of the code that aren't Flask endpoints can also
    re-use the logic to create/get a SH session

--- authentication.py
+++ authentication.py
@@ -26,8 +26,7 @@ CLIENT_CERT_AUTH_ENABLED = CERT_FILE_PATH and KEY_FILE_PATH
 
 SIGNINGHUB_SESSION_BASE_URI = KALEIDOS_RESOURCE_BASE_URI + "id/signinghub-sessions/"
 
-def ensure_signinghub_session(mu_session_uri):
-    """For api interactions made through Kaleidos initiated by a user"""
+def get_or_create_signinghub_session(mu_session_uri):
     mu_session_query = construct_get_mu_session_query(mu_session_uri)
     mu_session_result = sudo_query(mu_session_query)['results']['bindings']
     if not mu_session_result:
@@ -35,13 +34,14 @@ def ensure_signinghub_session(mu_session_uri):
     mu_session = mu_session_result[0]
     sh_session_query = construct_get_signinghub_session_query(mu_session_uri)
     sh_session_results = sudo_query(sh_session_query)['results']['bindings']
+    sh_session = None
     if sh_session_results: # Restore SigningHub session
         log("Found a valid SigningHub session.")
         sh_session_result = sh_session_results[0]
-        g.sh_session = SigningHubSession(SIGNINGHUB_API_URL)
+        sh_session = SigningHubSession(SIGNINGHUB_API_URL)
         if CLIENT_CERT_AUTH_ENABLED:
-            g.sh_session.cert = (CERT_FILE_PATH, KEY_FILE_PATH) # For authenticating against VO-network
-        g.sh_session.access_token = sh_session_result["token"]["value"]
+            sh_session.cert = (CERT_FILE_PATH, KEY_FILE_PATH) # For authenticating against VO-network
+        sh_session.access_token = sh_session_result["token"]["value"]
     else: # Open new SigningHub session
         log("No valid SigningHub session found. Opening a new one ...")
         sh_session = open_new_signinghub_machine_user_session(mu_session["ovoCode"]["value"], mu_session["email"]["value"])
@@ -50,7 +50,12 @@ def ensure_signinghub_session(mu_session_uri):
         sudo_update(sh_session_query)
         sh_session_link_query = construct_attach_signinghub_session_to_mu_session_query(sh_session_uri, mu_session_uri)
         sudo_update(sh_session_link_query)
-        g.sh_session = sh_session
+    return sh_session
+
+def ensure_signinghub_session(mu_session_uri):
+    """For api interactions made through Kaleidos initiated by a user"""
+    sh_session = get_or_create_signinghub_session(mu_session_uri)
+    g.sh_session = sh_session
 
 def signinghub_session_required(f):
     @wraps(f)
